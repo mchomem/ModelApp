b2ff4254d3c00f03596076638e5aa6992e514ecc
Misael Homem <misael.homem@gmail.com>
2022-06-23 15:35:56 -0300

    Rename project

--- ModelApp.OldSite/Properties/AssemblyInfo.cs
+++ ModelApp.OldSite/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ModelApp.Site")]
+[assembly: AssemblyTitle("ModelApp.OldSite")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ModelApp.Site")]
+[assembly: AssemblyProduct("ModelApp.OldSite")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
